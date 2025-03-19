2104846a9c8dccb8b370b91c40a9c5deccb6ca02
Arin7712 <aringawande7712@gmail.com>
2025-03-19 12:40:53 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner"
 import type { Metadata } from "next";
 import { Anonymous_Pro, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const font = Anonymous_Pro({
   subsets: ["latin"],
@@ -20,6 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Quantumflux Technology</title>
+        <meta name="description" content="Transform ideas into innovative software and wesbites that drive success." />
+        <meta name="keywords" content="ERP Software, CRM Software, Desktop App, Android App, Websites, E-commerce Websites" />
+        <meta property="og:title" content="Quantumflux Technology" />
+        <meta property="og:description" content="Transform ideas into innovative software and wesbites that drive success." />
+        <meta property="og:image" content="/bg.webp" />
+        {/* <meta property="og:url" content="https://yourwebsite.com" /> */}
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      </Head>
       <body
         className={font.className}
       >
